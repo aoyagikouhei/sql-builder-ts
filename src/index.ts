8b597d68cfbb5595797672a4069ac7d0b2d57eda
aoyagikouhei <aoyagi.kouhei@gmail.com>
2021-05-17 08:35:01 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+class Users {
+    id: number;
+    name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    join(company: Company) {
+        
+    }
+}
+
+
+
+interface Company {
+    uuid: string,
+    name: string,
+}
+
+let msg = "テスト"
+let user = new Users(10, msg)
+console.log(user)
+
